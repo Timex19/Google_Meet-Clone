a9c70c7d9585d0d93cdc3d5fe949513736234db7
Timex19 <ojooluwatimilehinpeter@yahoo.com>
2022-08-20 00:34:33 +0100

    Creating the Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Header from './components/Header/Header';
+import { Header, Home } from './components';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Button, Divider, InputAdornment, TextField } from "@material-ui/core";
+import { Keyboard, VideoCallOutlined } from "@material-ui/icons";
+import "./styles.css";
+
+const Home = () => {
+  return (
+    <div className="hero">
+      <div className="hero__left">
+        <div className="hero__featureText">
+          <h1 className="hero__title">
+            Premium video meetings. Now free for everyone
+          </h1>
+          <p className="hero__subtitle">
+            We re-engineered the service we built for secure buisness meetings,
+            Google Meet, to make sure it free and available for all.
+          </p>
+        </div>
+
+        <div className="hero__buttons">
+          <Button
+            color="primary"
+            variant="contained"
+            className="hero__createBTN"
+          >
+            <VideoCallOutlined />
+            <p>New Meeting</p>
+          </Button>
+
+          <TextField
+            className="hero__input"
+            variant="outlined"
+            placeholder="Enter a code or a link"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <Keyboard className="icon" />
+                </InputAdornment>
+              ),
+            }}
+          />
+
+          <Button className="hero__joinBTN">Join</Button>
+        </div>
+
+        <Divider />
+
+        <p className="hero__learnMore">Learn more about Google Meet</p>
+      </div>
+
+      <div className="hero__right">
+        <img
+          className="hero__image"
+          src="https://www.gstatic.com/meet/google_meet_marketing_ongoing_meeting_grid_427cbb32d746b1d0133b898b50115e96.jpg"
+          alt="Feature Image"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
